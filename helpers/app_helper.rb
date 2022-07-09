0f10a2208a09c3eccce9c389b9521f3169dbf05b
Godwin Nwachukwu <nwgwin@gmail.com>
2022-07-09 09:34:40 +0100

    include reviewers recommendations

--- helpers/app_helper.rb
+++ helpers/app_helper.rb
@@ -110,7 +110,7 @@ module AppHelper
     until answered
       puts 'Please Select a person using the selection number for the person (number to the left of the name)'
       @rentals.map(&:person).to_set.each_with_index do |person, idx|
-        puts "Selection #{idx} - #{person[:name]}"
+        puts "Selection #{idx} - #{person.name}"
       end
       selection = gets.chomp.to_i
       return @rentals[selection].person.id if selection.between?(0, max)
